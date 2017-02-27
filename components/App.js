4d86ff5670732bc64d5cdbb6d9df4c09cf5ff999
belhalaribi <laribibelha@gmail.com>
2017-02-27 11:50:47 +0100

    chem

---
+++ components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import s from '../styles/main.sass';
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        hello rojlaazzzzzzzzzz
+      </div>
+    )
+  }
+}
+
+render(<App/>, document.getElementById('app'));
