18fe8ed97c6b328f48a6fbf92eb79403614d7624
Bill <zohlouf@hotmail.com>
2017-02-28 09:23:56 +0100

    day1

--- components/App.js
+++ components/App.js
@@ -1,15 +1,47 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
+import Comp1 from './Comp1';
 import s from '../styles/main.sass';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      degla: 5,
+      numbers: [6],
+    }
+  }
+
+  clickMe(n) {
+    let newN = this.state.numbers;
+    let newD = this.state.degla + n;
+
+    newN.push(newD);
+
+    this.setState({
+      degla: newD,
+      numbers: newN,
+    })
+  }
+
+  getNumbers(){
+    let res = this.state.numbers.map(function(item, index){
+      return(
+        <button key={"btn"+index}>{item}</button>
+      )
+    })
+    return res;
+  }
+
   render() {
     return(
-      <div>
-        hello
-        bilel
-
+      <div class="root">
+        <div
+          class="buttonz"
+          onClick={this.clickMe.bind(this, 5)}
+        >click me</div>
+        {this.state.degla}
+        {this.getNumbers()}
       </div>
     )
   }
---
+++ components/Comp1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Comp1 extends Component
+{
+  render(){
+    return(
+      <div>
+        Comp1
+      </div>
+    )
+  }
+}
